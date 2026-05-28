ad3d43625754a06ceae12a374d11eb80a7d534e4
ToKiNoBug <tokinobug@163.com>
2026-05-28 11:46:38 +0800

    Add copyright declarations to files added since this fork
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- src/thermophysicalModels/specie/equationOfState/cubicEOS.C
+++ src/thermophysicalModels/specie/equationOfState/cubicEOS.C
@@ -1,6 +1,22 @@
-//
-// Created by joseph on 25-7-15.
-//
+/*---------------------------------------------------------------------------*\
+License
+    This file is part of Mikeno. Mikeno (https://github.com/nnSemenov/Mikeno) is
+    a fork of OpenFOAM (https://github.com/OpenFOAM/OpenFOAM-dev).
+
+    Mikeno is free software: you can redistribute it and/or modify it  under the
+    terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later
+    version.
+
+    Mikeno is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+    details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mikeno.  If not, see <http://www.gnu.org/licenses/>.
+\*---------------------------------------------------------------------------*/
+
 #include "cubicEOS.H"
 #include "dictionary.H"
 
