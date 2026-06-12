4eea315bd52d3a1c4af7e36d2e5297e77da78556
ToKiNoBug <tokinobug@163.com>
2026-06-12 10:30:01 +0800

    phaseSystem: Add Gnielinski correlation for packed bed heat transfer
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

---
+++ applications/modules/multiphaseEuler/phaseSystem/interfacialModels/heatTransferModels/Gnielinski/Gnielinski.C
@@ -0,0 +1,73 @@
+/*
+License
+    This file is part of Mikeno. Mikeno (https://github.com/nnSemenov/Mikeno) is
+    a fork of OpenFOAM (https://github.com/OpenFOAM/OpenFOAM-dev).
+
+    Mikeno is free software: you can redistribute it and/or modify it  under the
+    terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later
+    version.
+
+    Mikeno is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+    details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mikeno.  If not, see <http://www.gnu.org/licenses/>.
+\*---------------------------------------------------------------------------*/
+
+#include "Gnielinski.H"
+#include "addToRunTimeSelectionTable.H"
+
+namespace Foam::heatTransferModels
+{
+defineTypeNameAndDebug(Gnielinski, 0);
+addToRunTimeSelectionTable(heatTransferModel, Gnielinski, dictionary);
+} // namespace Foam::heatTransferModels
+
+Foam::heatTransferModels::Gnielinski::Gnielinski(
+    const dictionary &dict, const phaseInterface &interface,
+    const bool registerObject)
+    : heatTransferModel{dict, interface, registerObject},
+      interface_(
+          interface.modelCast<heatTransferModel, dispersedPhaseInterface>()),
+      sphere_{dict.lookup<bool>("sphere")}, shapeFactor_{1},
+      residualRe_{dict.lookupOrDefault<scalar>("residualRe", 1e-6)}
+{
+    if (residualRe_ <= 0) {
+        FatalErrorInFunction << "Found invalid residualRe = " << residualRe_
+                             << ", expected to be small positive number."
+                             << exit(FatalError);
+    }
+
+    if (not sphere_) {
+        shapeFactor_ = dict.lookup<scalar>("factor");
+    }
+}
+
+
+Foam::tmp<Foam::volScalarField> Foam::heatTransferModels::Gnielinski::K(
+    const scalar residualAlpha) const
+{
+    const volScalarField Re(max(interface_.Re(), residualRe_));
+    const volScalarField Pr(interface_.Pr());
+
+    auto Nu_laminar = 0.664 * sqrt(Re) * cbrt(Pr);
+    auto Nu_turbulence = 0.037 * pow(Re, 0.8) * Pr /
+        (1 + 2.443 * pow(Re, -0.1) * (pow(Pr, 2. / 3.) - 1));
+
+    auto Nu = 2 + sqrt(sqr(Nu_laminar) + sqr(Nu_turbulence));
+
+    if (sphere_) {
+        const volScalarField &void_fraction = interface_.continuous();
+        Nu.ref() *= (1 + 1.5 * (1 - void_fraction));
+    }
+    else {
+        Nu.ref() *= shapeFactor_;
+    }
+
+    return 6 * max(interface_.dispersed(), residualAlpha) *
+        interface_.continuous().thermo().kappa() * Nu /
+        sqr(interface_.dispersed().d());
+}
