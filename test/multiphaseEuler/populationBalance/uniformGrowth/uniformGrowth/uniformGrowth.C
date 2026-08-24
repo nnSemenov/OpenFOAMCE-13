bd2153b46db3613d147710aeb13fd437d47a4da4
ToKiNoBug <tokinobug@163.com>
2026-08-24 14:44:02 +0800

    Fix functional test: multiphaseEuler/populationBalance/uniformGrowth
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- test/multiphaseEuler/populationBalance/uniformGrowth/uniformGrowth/uniformGrowth.C
+++ test/multiphaseEuler/populationBalance/uniformGrowth/uniformGrowth/uniformGrowth.C
@@ -273,12 +273,12 @@ Foam::tmp<Foam::volInternalScalarField> Foam::fv::uniformGrowth::S
 
     forAll(popBal_.fs(), i)
     {
-        tmp<volInternalScalarField> tN =
-            popBal_.phases()[i]()*popBal_.f(i)/popBal_.v(i);
+        volInternalScalarField tN =
+            popBal_.phases()[i]*popBal_.f(i)/popBal_.v(i);
 
         if (&popBal_.phases()[i] == &phase)
         {
-            tSumNPhase.ref() += tN();
+            tSumNPhase.ref() += tN;
         }
 
         tSumN.ref() += tN;
